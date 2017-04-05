29adc3785f8dc7811e02a54b012d40bb4a30b70f
jerry <jdconet@qq.com>
2017-04-05 22:29:19 +0800

    admin

---
+++ application/admin/controller/Admin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jerry
+ * Date: 2017-04-05
+ * Time: 22:24
+ */
+
+namespace app\admin\controller;
+
+
+class Admin
+{
+    public function index(){
+        return "hello git";
+    }
+}
